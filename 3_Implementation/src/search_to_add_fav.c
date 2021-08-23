2d5ea2ae6c505766a225da66dd113c44809da6d9
G Shobitha <47855937+Shobzy@users.noreply.github.com>
2021-08-23 19:23:59 +0530

    Update search_to_add_fav.c

--- 3_Implementation/src/search_to_add_fav.c
+++ 3_Implementation/src/search_to_add_fav.c
@@ -17,15 +17,17 @@ int i,n,ch,l,found;
 
 void search_to_add_fav()
 {
-   fflush(stdin);
-        printf("\n\n\t..::ADD AN EXISTING CONTACT TO FAVOURITES\n\t==========================\n\t..::Enter the name of contact to add to favourite:");
-        scanf("%[^\n]",&name);
-        fp=fopen("contact.dll","r");
-        fr=fopen("fav_contacts.dll","w");
-        while(fread(&list,sizeof(list),1,fp)!=0)
-            if (stricmp(name,list.name)!=0)
+    fflush(stdin);
+    printf("\n\n\t..::ADD AN EXISTING CONTACT TO FAVOURITES\n\t==========================\n\t..::Enter the name of contact to add to favourite:");
+    scanf("%[^\n]",&name);
+    fp=fopen("contact.dll","r");
+    fr=fopen("fav_contacts.dll","a");
+    while(fread(&list,sizeof(list),1,fp)!=0)
+          if (stricmp(name,list.name)==0)
                 fwrite(&list,sizeof(list),1,fr);
+                
         
-        fclose(fr);
+    fclose(fr);
+    fclose(fp);
        
 }
